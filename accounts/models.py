a7d13ffc9d01de9e5920fa8130c8fc1ee0e5c3be
Rommelo Balandra <rommelo.b@gmail.com>
2026-05-06 22:54:26 -0700

    feat: complete Phase 1 - setup custom user, postgres, and JWT auth

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Define the role choices
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('staff', 'Staff'),
+        ('user', 'User'),
+    )
+    
+    # Add the custom role field, defaulting to a standard user
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-Party Apps
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'drf_spectacular',
+    
+    # Local Apps
+    'accounts',
+    'habits',
 ]
 
 MIDDLEWARE = [
@@ -74,8 +83,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'habit_tracker_db',
+        'USER': 'postgres',          # Replace with your Postgres username
+        'PASSWORD': 'dataWATbase26!',  # Replace with your Postgres password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -115,3 +128,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
