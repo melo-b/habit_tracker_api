f4370479ad44b7413aab061b85367fd951107c62
Rommelo Balandra <rommelo.b@gmail.com>
2026-07-13 18:11:16 -0700

    updated settings.py and django-test.yml with DB_host environment variable

--- core/settings.py
+++ core/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': '5432',
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
